f6710b03ddfe5d8fdf2abdf0809fe0bb052f450c
James W. Lane <james.w.lane@mac.com>
2019-01-02 15:47:17 -0600

    Removed dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
 const config = require('./data/config')
 
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
   siteMetadata: {
     title: config.defaultTitle,
