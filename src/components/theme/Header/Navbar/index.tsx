bcbb293298cf67d21b5013277d85d213ffa15fa7
James W. Lane <james.w.lane@mac.com>
2019-01-05 12:02:40 -0600

    Changed John to James

--- src/components/theme/Header/Navbar/index.tsx
+++ src/components/theme/Header/Navbar/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { Container } from '../../../common'
+import { Container } from 'Common'
 import NavbarLinks from '../NavbarLinks'
 import { Wrapper } from './styles'
 
